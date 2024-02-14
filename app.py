3896cf4325e25f6fd546086d71e618f0cf8c6ca1
Ankit Baliyan <86275544+AnkitBaliyan1@users.noreply.github.com>
2024-02-14 10:15:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,50 +1,76 @@
 import streamlit as st
 import os
-#from dotenv import load_dotenv
 from utils import *
 import uuid
 
-#load_dotenv()
-
-st.title("Custom Chatbot")
-
-if 'OPENAI_API_KEY' not in st.session_state:
-    st.session_state["OPENAI_API_KEY"]=''
-
-os.environ['HUGGINGFACEHUB_API_TOKEN']=os.environ.get("HUGGINGFACE_TOKEN")
-
-st.sidebar.title("😎🗝️")
-st.session_state["OPENAI_API_KEY"]=st.sidebar.text_input("Enter your API key?", type="password")
-# st.session_state["OPENAI_API_KEY"] = "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"
-os.environ["OPENAI_API_KEY"] = st.session_state["OPENAI_API_KEY"]
-file = st.sidebar.file_uploader("Upload your file", type="pdf")
 
 
-if "unique_id" not in st.session_state:
-    st.session_state["unique_id"]=''
+def main():
+    st.title("Custom Chatbot")
 
-st.session_state["unique_id"] = uuid.uuid4().hex
-id = st.session_state["unique_id"]
+    if 'OPENAI_API_KEY' not in st.session_state:
+        st.session_state["OPENAI_API_KEY"]=''
+    if "unique_id" not in st.session_state:
+        st.session_state["unique_id"]=''
+    if "bot_ready_flag" not in st.session_state:
+        st.session_state["bot_ready_flag"]=''
 
 
-st.subheader("Search Document..")
+    st.sidebar.title("😎🗝️")
 
-query = st.text_area("Enter query here..", key='question')
+    st.session_state["OPENAI_API_KEY"]=st.sidebar.text_input("Enter your API key?", type="password")
+    os.environ["OPENAI_API_KEY"] = st.session_state["OPENAI_API_KEY"]
+    
 
-submit = st.button("Get Answer")
+    st.session_state["unique_id"] = uuid.uuid4().hex
+    unique_id = st.session_state["unique_id"]
 
-if submit:
-    if file and query:
-        st.write("lookign for:",query)
-        directory = f"pdf_files/{id}"
-        save_pdf_to_directory(file,directory)
-        documents = load_docs(directory)
-        st.write(documents)
+    files = st.sidebar.file_uploader("Upload your file", type="pdf",accept_multiple_files=True)
+
+
+    push_doc = st.sidebar.button("Push for bot to learn")
+    if push_doc:
+        st.session_state["bot_ready_flag"] = False
+        # create doc out of provided pdf
+        docs = create_docs(files,unique_id)
+        global tiny_docs
+        tiny_docs = split_docs(docs, chunk_size=500, chunk_overlap=40)
+
+        # creating embeddings
+        embedding = get_embeddings()
+
+        with st.spinner("Wait! ChatBot is Learning ✋🏻"):
+            push_to_pinecone(tiny_docs, embedding)
+            st.session_state["bot_ready_flag"] = True
+
+
+
+    st.subheader("Search Document..🤖")
+
+    global query
+    query = st.text_area("Enter query here.. 👇🏻", key='question')
+
+    submit = st.button("Get Answer")
+
+    if submit and st.session_state["bot_ready_flag"]==True:
+        if query:
+            # text embedding
+            embedding = get_embeddings()
+
+            # get similar docs
+            with st.spinner("Generating Response"):
+                response = get_answer(query, embedding, k=2)
+
+                st.write(response)
+            st.success("How was that?")
+            
         
-    elif file:
-        st.error("Enter the query first")
-    elif query:
-        st.error("Enter the document first")
-    else:
-        st.error("You gotta be kidding me.. I really wish I could read your mind")
-        
+        else:
+            st.error("You gotta be kidding me.. I really wish I could read your mind")
+    elif submit:
+        st.error("Provide the document first")
+    
+
+
+if __name__ == "__main__":
+    main()
